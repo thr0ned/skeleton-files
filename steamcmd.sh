82cd58b85130c6b26c91257b7716756f161d9ceb
thr0ned <nofragsthrone@gmail.com>
2025-09-17 02:45:38 +0200

    codfjsahoijhds

--- steamcmd.sh
+++ steamcmd.sh
@@ -1,46 +1,48 @@
 #!/usr/bin/env bash
 
+# Set Steam root explicitly
 STEAMROOT="/home/vadrigar/.steam"
-STEAMCMD=`basename "$0" .sh`
+mkdir -p "$STEAMROOT"
 
-UNAME=`uname`
+STEAMCMD=$(basename "$0" .sh)
+
+UNAME=$(uname)
 if [ "$UNAME" == "Linux" ]; then
-  STEAMEXE="${STEAMROOT}/linux32/${STEAMCMD}"
-  PLATFORM="linux32"
-  export LD_LIBRARY_PATH="$STEAMROOT/$PLATFORM:$LD_LIBRARY_PATH"
-else # if [ "$UNAME" == "Darwin" ]; then
-  STEAMEXE="${STEAMROOT}/${STEAMCMD}"
-  if [ ! -x ${STEAMEXE} ]; then
-    STEAMEXE="${STEAMROOT}/Steam.AppBundle/Steam/Contents/MacOS/${STEAMCMD}"
-  fi
-  export DYLD_LIBRARY_PATH="$STEAMROOT:$DYLD_LIBRARY_PATH"
-  export DYLD_FRAMEWORK_PATH="$STEAMROOT:$DYLD_FRAMEWORK_PATH"
+    PLATFORM="linux32"
+    STEAMEXE="$STEAMROOT/$PLATFORM/$STEAMCMD"
+    export LD_LIBRARY_PATH="$STEAMROOT/$PLATFORM:${LD_LIBRARY_PATH-}"
+else
+    STEAMEXE="$STEAMROOT/$STEAMCMD"
+    if [ ! -x "$STEAMEXE" ]; then
+        STEAMEXE="$STEAMROOT/Steam.AppBundle/Steam/Contents/MacOS/$STEAMCMD"
+    fi
+    export DYLD_LIBRARY_PATH="$STEAMROOT:${DYLD_LIBRARY_PATH-}"
+    export DYLD_FRAMEWORK_PATH="$STEAMROOT:${DYLD_FRAMEWORK_PATH-}"
 fi
 
 ulimit -n 2048
-
 MAGIC_RESTART_EXITCODE=42
 
+# Optional debugger support
 if [ "$DEBUGGER" == "gdb" ] || [ "$DEBUGGER" == "cgdb" ]; then
-  ARGSFILE=$(mktemp $USER.steam.gdb.XXXX)
+    ARGSFILE=$(mktemp "$USER.steam.gdb.XXXX")
 
-  # Set the LD_PRELOAD varname in the debugger, and unset the global version.
-  if [ "$LD_PRELOAD" ]; then
-    echo set env LD_PRELOAD=$LD_PRELOAD >> "$ARGSFILE"
-    echo show env LD_PRELOAD >> "$ARGSFILE"
-    unset LD_PRELOAD
-  fi
+    if [ "$LD_PRELOAD" ]; then
+        echo set env LD_PRELOAD=$LD_PRELOAD >> "$ARGSFILE"
+        echo show env LD_PRELOAD >> "$ARGSFILE"
+        unset LD_PRELOAD
+    fi
 
-  $DEBUGGER -x "$ARGSFILE" "$STEAMEXE" "$@"
-  rm "$ARGSFILE"
+    $DEBUGGER -x "$ARGSFILE" "$STEAMEXE" "$@"
+    rm "$ARGSFILE"
 else
-  $DEBUGGER "$STEAMEXE" "$@"
+    $DEBUGGER "$STEAMEXE" "$@"
 fi
 
 STATUS=$?
-
 if [ $STATUS -eq $MAGIC_RESTART_EXITCODE ]; then
     exec "$0" "$@"
 fi
+
 exit $STATUS
 
